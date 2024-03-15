e2fd3b84f481edb492ba4d6e5ad28ceba2ea5c15
mjkloeckner <martinjkloeckner@gmail.com>
2024-03-15 12:26:01 -0300

    Add solution for ej12

---
+++ solutions/ej12.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ctime>
+
+int main (void) {
+	unsigned long in_time_secs;
+
+	std::cin >> in_time_secs;
+
+	time_t seconds(in_time_secs);
+	tm *p = gmtime(&seconds);
+
+	std::cout << p->tm_yday << " days, "
+		<< p->tm_hour << ":" << p->tm_min << ":" << p->tm_sec << std::endl;
+
+	return 0;
+}
