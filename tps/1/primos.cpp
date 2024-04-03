00bd6ae511b1241a290d680687ce133d0f5cfec6
Martin Kloeckner <mjkloeckner@gmail.com>
2024-04-03 13:00:33 -0300

    Stop including `cmath` for calculating `sqrt(MAXIMO)`

--- tps/1/primos.cpp
+++ tps/1/primos.cpp
@@ -6,14 +6,13 @@
 #include <fstream>
 #include <iomanip>
 #include <vector>
-#include <cmath>
 
 #define OUTPUT_FILE_PATH "primos.txt"
 const unsigned int MAXIMO = 100000000;
 
 void vectorDiscardNonPrimes(std::vector<bool>& v) {
     v[0] = v[1] = false; // 0 y 1 no son primos
-    for (size_t i = 2; i < std::sqrt(MAXIMO); ++i) {
+    for (size_t i = 2; i*i < MAXIMO; ++i) {
         if(v[i]) {
             for (size_t j = i; j <= (MAXIMO/i); ++j) {
                 v[i*j] = false;
@@ -43,7 +42,6 @@ int main (void) {
     std::vector<bool> numeros(MAXIMO, true);
 
     ti = clock();
-
     vectorDiscardNonPrimes(numeros);
 
     fp.open(OUTPUT_FILE_PATH);
