17736b54dd4990463397872b716e697644774e96
Ignacio Cettour <129465157+IgnacioCettour@users.noreply.github.com>
2024-06-16 16:44:44 -0300

    tdas

---
+++ Barrio.h
@@ -0,0 +1,54 @@
+/*
+ * Barrio.h
+
+ *
+ *  Created on: 16/06/2024
+ *      Author: algo2
+ */
+
+#include <string>
+#include <list.h>
+#include <Parada.h>
+#ifndef BARRIO_H_
+#define BARRIO_H_
+
+
+class Barrio {
+private:
+
+	string nombre;
+	int comuna;
+	list<Parada*>* paradas;
+
+public:
+	/*
+	 *post: Deja creado la clase con los parametros dados
+	 */
+	Barrio(string nombre, int comuna);
+
+	/*
+	 * post: Vacia y elimina el Barrio
+	 */
+	virtual ~Barrio();
+	/*
+	 * post: Devuelve el numero de comuna del Barrio
+	 */
+	int getComuna();
+
+	/*
+	 * post: Devuelve el nombre del Barrio
+	 */
+	string getNombre();
+
+	/*
+	 * post: Devuelve una lista con todos las paradas de ese Barrio
+	 */
+	list<Parada*>* obtenerParadas();
+
+	/*
+	 * post: Agrega una para nueva
+	 */
+	void appendParada(string calle, int alturaPlano, string direccion, unsigned int coord_x, unsigned int coord_y);
+};
+
+#endif /* BARRIO_H_ */
---
+++ Linea.h
@@ -0,0 +1,45 @@
+/*
+ * Linea.h
+ *
+ *  Created on: 16/06/2024
+ *      Author: algo2
+ */
+
+#include <string>
+#ifndef LINEA_H_
+#define LINEA_H_
+
+};
+
+class Linea {
+private:
+
+	int numero;
+	bool sentido; // aca puede ser tambien un enum ?????? un bool con true y false porque en realidad es I o V
+
+public:
+
+	/*
+	 * pre: Recibe como parametro el numero y el sentido de la linea
+	 * post: crea la clase Linea con su numero y sentido
+	 */
+	Linea(int numero, bool sentido);
+
+	/*
+	 * post: Devuelve el numero de la linea
+	 */
+	int getNumero();
+
+	/*
+	 * post: Devuelve el sentido de la linea
+	 */
+	 bool getSentido();
+
+
+	/*
+	 *  post: Elimina la linea
+	 */
+	virtual ~Linea();
+};
+
+#endif /* LINEA_H_ */
